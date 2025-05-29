68b9435d7f8200a8702ff9c49918b55517fb1b45
luca.moldovan <luca.moldovan07@htl-salzburg.ac.at>
2025-05-29 15:32:10 +0200

    added sceens

--- Assets/Scripts/Start_button.cs
+++ Assets/Scripts/Start_button.cs
@@ -14,5 +14,32 @@ public class Start_button : MonoBehaviour
         SceneManager.LoadScene("Scene1");
 
     }
-   
+    public void Wire_ja_button()
+    {
+        SceneManager.LoadScene("Scene2");
+
+    }
+    public void Wire_Nein_button()
+    {
+        SceneManager.LoadScene("Scene2");
+
+    }public void Wire_3_button()
+    {
+        SceneManager.LoadScene("3_wire");
+
+    }public void Wire_4_button()
+    {
+        SceneManager.LoadScene("4_wire");
+
+    }public void Wire_5_button()
+    {
+        SceneManager.LoadScene("5_wire");
+
+    }public void Wire_6_button()
+    {
+        SceneManager.LoadScene("6_wire");
+
+    }
+
+
 }
